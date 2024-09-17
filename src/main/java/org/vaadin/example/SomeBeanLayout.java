ddcfdcec6ab0acc0e2fba1845d0bb7e90d587eb0
Johannes Tuikkala <johannes@vaadin.com>
2024-09-17 10:41:09 +0300

    Trying with the RouteScoped bean and custom flow-server

--- src/main/java/org/vaadin/example/SomeBeanLayout.java
+++ src/main/java/org/vaadin/example/SomeBeanLayout.java
@@ -1,20 +1,22 @@
 package org.vaadin.example;
 
 import com.vaadin.flow.server.VaadinSession;
-import com.vaadin.flow.spring.annotation.SpringComponent;
+import com.vaadin.flow.spring.annotation.RouteScope;
+import com.vaadin.flow.spring.annotation.RouteScopeOwner;
 import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
 
-@SpringComponent
-@Scope(value = BeanDefinition.SCOPE_PROTOTYPE)
+@Component
+@Scope("prototype")
 public class SomeBeanLayout implements Serializable {
 
     private final transient VaadinUiScopeBean vaadinUiScopeBean;
 
-    public SomeBeanLayout(VaadinUiScopeBean vaadinUiScopeBean) {
+    public SomeBeanLayout(@Autowired @RouteScopeOwner(MainView.class)VaadinUiScopeBean vaadinUiScopeBean) {
         this.vaadinUiScopeBean = vaadinUiScopeBean;
     }
 
--- src/main/java/org/vaadin/example/VaadinUiScopeBean.java
+++ src/main/java/org/vaadin/example/VaadinUiScopeBean.java
@@ -1,12 +1,14 @@
 package org.vaadin.example;
 
-import com.vaadin.flow.spring.annotation.UIScope;
+import com.vaadin.flow.spring.annotation.RouteScope;
+import com.vaadin.flow.spring.annotation.RouteScopeOwner;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
 
 @Component
-@UIScope
+@RouteScope
+@RouteScopeOwner(MainView.class)
 public class VaadinUiScopeBean implements Serializable {
 
     public void testMe() {
